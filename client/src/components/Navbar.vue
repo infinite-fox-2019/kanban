8ff66a7c43695278d8bb9f51092341d9bc622936
laskarks <laskar.ksatria12@gmail.com>
2019-10-20 21:14:24 -0400

    Before-9AM

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="navbar navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">KanGaban</a>
+  <Modal/>
+</nav>
+</template>
+
+<script>
+import Modal from '../components/Modal'
+export default {
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+</style>
